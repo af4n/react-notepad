f2b8ef44c3cab3649674dcca6b7a9eaa019be03a
af4n <aleksey.afanaskin@gmail.com>
2020-10-23 15:37:42 +0300

    deploy project

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+export const Navbar = () => (
+  <nav className={"navbar navbar-dark navbar-expand-lg bg-dark text-light"}>
+    <div className="navbar-brand mr-4">
+      Notepad
+    </div>
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <NavLink
+          className="nav-link"
+          to="/"
+          exact
+        >Home</NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          className="nav-link"
+          to="/about"
+        >About</NavLink>
+      </li>
+    </ul>
+  </nav>
+)
---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const About = () => (
+  <div className="jumbotron">
+    <div className="container">
+      <h1 className="display-4">Notepad React App</h1>
+      <p className="lead">Version 1.0.0</p>
+    </div>
+  </div>
+)
